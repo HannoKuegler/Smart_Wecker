4a37bf4f96f338844e956873e4bb4292ef716f88
Hanno Kügler <hannokuegler@gmailcom>
2020-04-02 18:35:51 +0200

    ESP

--- Smart Wecker/src/main.cpp
+++ Smart Wecker/src/main.cpp
@@ -221,12 +221,7 @@ void printTime() {
 
 
 // Sommerzeit rechnen
-//-------- utility code from jurs ----------
-boolean summertime_EU(int year, byte month, byte day, byte hour, byte tzHours)
-// European Daylight Savings Time calculation by "jurs" for German Arduino Forum
-// input parameters: "normal time" for year, month, day, hour and tzHours (0=UTC, 1=MEZ)
-// return value: returns true during Daylight Saving Time, false otherwise
-{
+boolean summertime_EU(int year, byte month, byte day, byte hour, byte tzHours){
   if (month < 3 || month > 10) return false; // keine Sommerzeit in Jan, Feb, Nov, Dez
   if (month > 3 && month < 10) return true; // Sommerzeit in Apr, Mai, Jun, Jul, Aug, Sep
   if (month == 3 && (hour + 24 * day) >= (1 + tzHours + 24 * (31 - (5 * year / 4 + 4) % 7)) || month == 10 && (hour + 24 * day) < (1 + tzHours + 24 * (31 - (5 * year / 4 + 1) % 7)))
